d17074d8fc9247f348ef1a26ae9b594227aa28bd
metalex9 <alexbainter@gmail.com>
2021-02-24 18:25:15 -0600

    Green linter

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,8 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+//eslint-disable-next-line
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
