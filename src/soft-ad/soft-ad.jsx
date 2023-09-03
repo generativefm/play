8bd7d3569dfcb1950dda97f5bb562c90341ee18e
alexbainter <alexbainter@gmail.com>
2023-09-03 13:26:25 -0500

    Only open soft ad in new tab if something is playback is stopped

--- src/soft-ad/soft-ad.jsx
+++ src/soft-ad/soft-ad.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import selectPlaybackStatus from '../playback/select-playback-status';
 import styles from './soft-ad.module.scss';
 
 const SoftAd = ({ children, href }) => {
+  const isStopped = useSelector(selectPlaybackStatus) === 'stopped';
   return (
     <a
       className={styles['soft-ad']}
       href={href}
-      target="_blank"
-      rel="noreferrer noopener"
+      target={isStopped ? '' : '_blank'}
+      rel={isStopped ? '' : 'noreferrer noopener'}
     >
       {children}
     </a>
